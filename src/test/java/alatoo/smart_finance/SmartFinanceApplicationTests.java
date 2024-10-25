260cf075dae26c8e3ea71ea09d5cee49f5e3e8a7
Iskha-69 <iskhak.koichubaev@alatoo.edu.kg>
2024-10-25 11:14:25 +0600

    first commit

---
+++ src/test/java/alatoo/smart_finance/SmartFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package alatoo.smart_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
